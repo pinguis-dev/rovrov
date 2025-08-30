f3599d7a19343c079cb2f32e752febe60c39ef89
pinguis-dev <pinguis.dev@gmail.com>
2025-08-30 16:59:36 +0900

    assets: point default avatar to assets/images/default-avatar.jpg (RN require with eslint guards)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,5 @@
 // 画像・フォント等
+
+// Default avatar image
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, global-require
+export const defaultAvatar = require('../../assets/images/default-avatar.jpg');
